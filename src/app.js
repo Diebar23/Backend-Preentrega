65370dae0265aada0f82942f29ae7d45080a0916
Diego Barbas <barbasdiego@yahoo.com>
2024-02-17 18:18:13 -0300

    Segunda Pre entrega

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
-const socket = require("socket.io");
 const PUERTO = 8080;
 require("./database.js"); //Inicializador de datos
 
@@ -24,36 +23,8 @@ app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
 
-app.get("/api/products", async (req, res) => {
-    const page = req.query.page || 1;
-    const limit = 2; 
-
-    try {
-        const productsList = await productModel.paginate({}, {limit, page})
-
-        const productFinal = productsList.docs.map(pizza => {
-            const {_id, ...rest} = product.toObject();
-            return rest;
-        })
-        
-
-        res.render("products", {
-            product: productFinal,
-            hasPrevPage: productsList.hasPrevPage,
-            hasNextPage: productsList.hasNextPage,
-            prevPage: productsList.prevPage, 
-            nextPage: productsList.nextPage,
-            currentPage: productsList.page,
-            totalPages: productsList.totalPages
-        });
-    } catch (error) {
-        console.log("Error en la paginacion ", error); 
-        res.status(500).send("Error en el server");
-    }
-})
-
 //Listen
-const httpServer = app.listen(PUERTO, () => {
+app.listen(PUERTO, () => {
     console.log(`Servidor escuchando en el puerto ${PUERTO}`);
 
-})
+});
--- src/database.js
+++ src/database.js
@@ -1,10 +1,10 @@
 //Acá hacemos la conexión con MONGODB
 
-//1) Instalar mongoose: npm i mongoose
+//Instalar mongoose: npm i mongoose
 
 const mongoose = require("mongoose");
 
-//2) Nos conectamos a la BD: 
+//conexión a la BD: 
 
 mongoose.connect("mongodb+srv://barbasdiego:cursocoder@cluster0.6f0lw9x.mongodb.net/ecommerce?retryWrites=true&w=majority")
     .then(() => console.log("Conexión exitosa"))
